fe009862ee5b4a78a2e0400b93a6a39674808492
DanielMelloFarias <danielmellofarias@gmail.com>
2024-09-01 11:14:46 -0300

    troquei pra de main para index

---
+++ api/index.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+import os
+
+app = FastAPI()
+
+# Monta a pasta 'static' para servir arquivos estáticos como o index.html
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root():
+    # Carrega o arquivo index.html que estará na pasta 'static'
+    with open("static/index.html", "r") as file:
+        return file.read()
+
+# Importando os endpoints de cada API
+from verificar_pdf_a4_paginas import verificar_pdf_a4_paginas
+from verificar_capa import verificar_capa
+from verificar_fontes_tamanhos import verificar_fontes_tamanhos
+
+# Monta as rotas para as APIs
+app.include_router(verificar_pdf_a4_paginas.router)
+app.include_router(verificar_capa.router)
+app.include_router(verificar_fontes_tamanhos.router)
